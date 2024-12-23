9380e88a730c939642428dc1d17d1f6a7860699b
Khare <anand.khare@siemensgamesa.com>
2024-12-23 15:13:45 +0530

    new case

--- test_models.py
+++ test_models.py
@@ -8,23 +8,23 @@ class TestClass(unittest.TestCase):
     # Get the current working directory
     CURRENT_DIRECTORY = os.getcwd()
 
-    def test_loan_pred_model(self):
-        # Load Model
-        model_path = os.path.join(self.CURRENT_DIRECTORY, 'LoanPredictor', 'RF_Loan_Model.pkl')
-        model = pickle.load(open(model_path, 'rb'))
-        data = {
-                "Gender": 1,  "Married": 1,  "Dependents": 1,  "Education": 1,  "Self_Employed": 0,  "LoanAmount": 120,
-                "Loan_Amount_Term": 360,  "Credit_History": 0,  "Property_Area": 0,  "TotalIncome": 3000
-                }
-        
-        # Predict Loan Approval
-        prediction = model.predict([[
-                                    data['Gender'], data['Married'], data['Dependents'], data['Education'], data['Self_Employed'],
-                                    data['LoanAmount'], data['Loan_Amount_Term'], data['Credit_History'], data['Property_Area'], data['TotalIncome']
-                                    ]])
-
-        # Do assertions
-        self.assertFalse(prediction)
+    #def test_loan_pred_model(self):
+    #    # Load Model
+    #    model_path = os.path.join(self.CURRENT_DIRECTORY, 'LoanPredictor', 'RF_Loan_Model.pkl')
+    #    model = pickle.load(open(model_path, 'rb'))
+    #    data = {
+    #            "Gender": 1,  "Married": 1,  "Dependents": 1,  "Education": 1,  "Self_Employed": 0,  "LoanAmount": 120,
+    #            "Loan_Amount_Term": 360,  "Credit_History": 0,  "Property_Area": 0,  "TotalIncome": 3000
+    #            }
+    #    
+    #    # Predict Loan Approval
+    #    prediction = model.predict([[
+    #                                data['Gender'], data['Married'], data['Dependents'], data['Education'], data['Self_Employed'],
+    #                                data['LoanAmount'], data['Loan_Amount_Term'], data['Credit_History'], data['Property_Area'], data['TotalIncome']
+    #                                ]])
+#
+    #    # Do assertions
+    #    self.assertFalse(prediction)
     
     def test_rainfall_trends(self):
         # Load Model
